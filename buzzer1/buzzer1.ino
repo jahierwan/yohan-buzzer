ea8a6726ee170dacc3f4437b7686a838c8b25ed6
slipyrai <ninejaille@gmail.com>
2024-02-06 11:00:47 +0100

    blocage de la boucle arduino lors d'un clique, deblocage depuis le telephone pour passer a la question suivante

--- buzzer1/buzzer1.ino
+++ buzzer1/buzzer1.ino
@@ -9,9 +9,12 @@ https://github.com/nkolban/esp32-snippets/blob/master/cpp_utils/tests/BLE%20Test
 #include <BLEDevice.h>
 #include <BLEUtils.h>
 #include <BLEServer.h>
+#include <string.h>
+#include <stdio.h>
 
 String from_tel;
-String num_bouton = "";
+String transfer = "";
+bool actif = true;
 
 #define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
 #define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
@@ -19,18 +22,20 @@ String num_bouton = "";
 class MyCallbacks: public BLECharacteristicCallbacks {
     void onWrite(BLECharacteristic *pCharacteristic) {
       std::string value = pCharacteristic->getValue();
-      pCharacteristic->setValue(num_bouton.c_str()); //
+      pCharacteristic->setValue(transfer.c_str()); //
 
       if (value.length() > 0) {
         from_tel = "";
         for (int i = 0; i < value.length(); i++){
           // Serial.print(value[i]); // Presenta value.
           from_tel = from_tel + value[i];
+        } 
+        if (from_tel == "true") {
+          actif = true;
         }
-        Serial.println("*********");
-        Serial.print("from_tel = ");
-        Serial.println(from_tel);
       }
+      
+      
     }
 };
 
@@ -62,25 +67,26 @@ void setup() {
 
 void loop() {
   int value;
-
-  // put your main code here, to run repeatedly:
-  delay(50);
-
   value = analogRead(PIN);
-  Serial.print("intensité sur le pin = ");
-  Serial.println(value);
-  if ( value < 100 ) {
-    num_bouton = "1";
-  } else if ( value < 600 ) {
-    num_bouton = "2";
-  } else if ( value < 1500 ) {
-    num_bouton = "3";
-  } else if ( value < 3000 ) {
-    num_bouton = "4";
-  } else {
-    num_bouton = "0";
-  };
-  Serial.print("Numero du bouton appuyé : ");
-  Serial.println(num_bouton);
+
+  if (value < 3000 & actif == true) {
+    Serial.print("intensité sur le pin = ");
+    Serial.println(value);
+    if ( value < 100 ) {
+      transfer = "1";
+    } else if ( value < 600 ) {
+      transfer = "2";
+    } else if ( value < 1500 ) {
+      transfer = "3";
+    } else {
+      transfer = "4";
+    };
+    Serial.print("Numero du bouton appuyé : ");
+    Serial.println(transfer);
+    actif = false;
+  }
+
+  
+  
 
 }
