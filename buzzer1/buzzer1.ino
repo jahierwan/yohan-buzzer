016428912576434c43e35efce2a56e3f2e5848f0
Erwan Jahier <erwan.jahier@univ-grenoble-alpes.fr>
2024-02-11 15:26:29 +0100

    Ca marche à peu pres (delay 1000?)
    de temps en temps, c'est le le jouer 't' qui a joué d'apres
    les telephone. pb d'affichage seulement ? Il semblerait

--- buzzer1/buzzer1.ino
+++ buzzer1/buzzer1.ino
@@ -12,29 +12,61 @@ https://github.com/nkolban/esp32-snippets/blob/master/cpp_utils/tests/BLE%20Test
 #include <string.h>
 #include <stdio.h>
 
-String from_tel;
+String qu_suivante;
 String transfer = "";
-bool actif = true;
+bool attente_buzz = true;
+bool debug = true;
 
 #define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
 #define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
 
-class MyCallbacks: public BLECharacteristicCallbacks {
-    void onWrite(BLECharacteristic *pCharacteristic) {
+
+// effet de bord : maj de valeur
+void recupere_valeur (BLECharacteristic *pCharacteristic) {
       std::string value = pCharacteristic->getValue();
-      pCharacteristic->setValue(transfer.c_str());
-      transfer = "";
-      
       if (value.length() > 0) {
-        from_tel = "";
+        qu_suivante = "";
         for (int i = 0; i < value.length(); i++){
           // Serial.print(value[i]); // Presenta value.
-          from_tel = from_tel + value[i];
-        } 
-        if (from_tel == "true") {
-          actif = true;
-        }
+          qu_suivante = qu_suivante + value[i];
+        };
+	if (debug) {
+	  Serial.print("recupere_valeur : qu_suivante = ");
+	  Serial.println(qu_suivante);
+	  Serial.print("tranfer = ");
+	  Serial.println(transfer);
+	  Serial.println("Fin recupere_valeur");
+	};
       }
+}
+
+void transferer (BLECharacteristic *pCharacteristic) {
+  Serial.print("J'envoie la valeur du bouton appuyé  : ");
+  Serial.println(transfer);
+  pCharacteristic->setValue(transfer.c_str());
+  transfer = "";
+
+}
+
+// appellé quand "question suivante" est pressé + toutes les secondes (via timer2)
+class MyCallbacks: public BLECharacteristicCallbacks {
+    void onWrite(BLECharacteristic *pCharacteristic) {
+
+      Serial.print("MyCallbacks: ");
+
+      if (transfer == "1" || transfer == "2" || transfer == "3" || transfer == "4" ) {
+	transferer(pCharacteristic);
+	delay(1000); //????
+      };
+      recupere_valeur(pCharacteristic);
+      if (qu_suivante == "t") {
+	  Serial.println("qu_suivante = true -> je met le mode 'attente du buzz' a true  ");
+	  attente_buzz = true;
+      } else {
+	Serial.print("qu_suivante n'est pas true. Il vaut : "); Serial.print(qu_suivante);
+      };
+      Serial.println("Fin de MyCallbacks ");
+
     }
 };
 
@@ -53,7 +85,7 @@ void setup() {
                                          BLECharacteristic::PROPERTY_WRITE
                                        );
   pCharacteristic->setCallbacks(new MyCallbacks());
-  pCharacteristic->setValue("");
+  pCharacteristic->setValue("7");
   pService->start();
 
   BLEAdvertising *pAdvertising = pServer->getAdvertising();
@@ -63,6 +95,7 @@ void setup() {
   pinMode(PIN, INPUT);
 }
 
+bool off = true;
 void loop() {
   int value;
 
@@ -70,7 +103,15 @@ void loop() {
   //Serial.print("intensité sur le pin = ");
   //Serial.println(value);
 
-  if (value < 3000 & actif == true) {
+  // debug
+  if (attente_buzz == false && off) {
+    Serial.print("loop : qu_suivante = ");
+    Serial.println(qu_suivante);
+    off = false;
+  };
+
+  if (value < 3000 & attente_buzz == true) {
+
     if ( value < 100 ) {
       transfer = "1";
     } else if ( value < 600 ) {
@@ -82,8 +123,7 @@ void loop() {
     };
     Serial.print("Numero du bouton appuyé : ");
     Serial.println(transfer);
-    actif = false;
+    attente_buzz = false;
+    off=true;
   }
-  //delay(40);  
-
 }
