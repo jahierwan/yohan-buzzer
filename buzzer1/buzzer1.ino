ee909d173a11e521ed972302fce437b2f86649d6
Erwan Jahier <erwan.jahier@univ-grenoble-alpes.fr>
2024-02-26 09:18:45 +0100

    ajout de locks

--- buzzer1/buzzer1.ino
+++ buzzer1/buzzer1.ino
@@ -17,6 +17,10 @@ String qu_suivante;
 String zone_partagee = "";
 bool attente_buzz = true;
 bool debug = true;
+bool lockJ1 = false;
+bool lockJ2 = false;
+bool lockJ3 = false;
+bool lockJ4 = false;
 
 #define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
 #define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
@@ -54,15 +58,20 @@ class MyCallbacks: public BLECharacteristicCallbacks {
       Serial.print("MyCallbacks: ");
 
       if (zone_partagee == "1" || zone_partagee == "2" || zone_partagee == "3" || zone_partagee == "4" ) {
-	transferer(pCharacteristic);
-	//delay(1000); //????
+        transferer(pCharacteristic);
+        //delay(1000); //????
       };
       recupere_valeur(pCharacteristic);
       if (qu_suivante == "t") {
-	  Serial.println("qu_suivante = t => je met le mode 'attente du buzz' a true");
-	  attente_buzz = true;
-      } else {
-	Serial.print("qu_suivante n'est pas true. Il vaut : "); Serial.print(qu_suivante);
+        Serial.println("qu_suivante = t => je met le mode 'attente du buzz' a true");
+        attente_buzz = true;
+        lockJ1 = false;
+        lockJ2 = false;
+        lockJ3 = false;
+        lockJ4 = false;
+      } else if (qu_suivante == "f"){ //si la reponse donné n'est pas bonne on peut à nouveau buzzer (mais le joueur sera lock)
+        attente_buzz = true;
+	      Serial.print("qu_suivante n'est pas true. Il vaut : "); Serial.print(qu_suivante);
       };
       Serial.println("Fin de MyCallbacks ");
     }
@@ -98,28 +107,30 @@ void loop() {
   int value;
 
   value = analogRead(PIN);
-  //Serial.print("intensité sur le pin = ");
-  //Serial.println(value);
+  Serial.print("intensité sur le pin = ");
+  Serial.println(value);
 
   if (value < 3000 & attente_buzz == true) {
-    if ( value < 100 ) {
+
+    if ( value < 50  & lockJ1 == false) {
       zone_partagee = "1";
-    } else if ( value < 600 ) {
+      lockJ1 = true;
+      attente_buzz = false;
+    } else if ( value < 1000 & value > 300 & lockJ2 == false) {
       zone_partagee = "2";
-    } else if ( value < 1500 ) {
+      lockJ2 = true;
+      attente_buzz = false;
+    } else if ( value < 1700 & value > 1100 & lockJ3 == false ) {
       zone_partagee = "3";
-    } else if (value < 3000) {
+      lockJ3 = true;
+      attente_buzz = false;
+    } else if (value < 3000 & value > 1800 & lockJ4 == false) {
       zone_partagee = "4";
+      lockJ4 = true;
+      attente_buzz = false;
     };
     Serial.print("Numero du bouton appuyé : ");
     Serial.println(zone_partagee);
-    attente_buzz = false;
-    off=true;
-  };
-  // pour le debug
-  if (attente_buzz == false && off) {
-    Serial.print("loop : qu_suivante = ");
-    Serial.println(qu_suivante);
-    off = false;
+    //attente_buzz = false;
   };
 }
